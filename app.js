f47726c88ef513faf2b384a779f8528cee4adc99
unknown <agassi.garry@gmail.com>
2022-06-19 13:39:26 +0700

    refactor

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 3001;
 const cors = require('cors');
 const mongo = require('mongodb').MongoClient;
-const client = require('socket.io')(4000);
+const socket = require('socket.io');
 const router = require('./routes');
+const server = app.listen(port, function () {
+  console.log('server running on port 3001');
+});
+
+app.use(cors);
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(router);
 mongo.connect(
   'mongodb+srv://vouchtesting:vouch12345@chatvouch.tapkwvx.mongodb.net/?retryWrites=true&w=majority',
   (err, db) => {
@@ -13,43 +21,21 @@ mongo.connect(
     }
 
     console.log('Connected to mongoDB');
-    client.on('connection', function () {
-      let chat = db.collection('chat');
 
-      sendStatus = function (s) {
-        socket.emit('status', s);
-      };
+    const io = socket(server, {
+      allowEIO3: true,
+      cors: { credentials: true, origin: 'http://localhost:8080' },
+    });
 
-      chat
-        .find()
-        .limit(100)
-        .sort({ _id: 1 })
-        .toArray(function (err, res) {
-          if (err) {
-            throw err;
-          }
-          socket.emit('output', res);
-        });
+    io.on('connection', function (socket) {
+      console.log('Socket connected');
 
-      socket.on('input', function (data) {
-        let { name, message, room } = data;
-        if (name === '' || message === '') {
-          sendStatus('Please enter a name or message');
-        } else {
-          chat.insert({ name, message, room }, function () {
-            client.emit('output', [data]);
-
-            sendStatus({
-              message: 'Message sent',
-              clear: true,
-            });
-          });
-        }
+      socket.on('testaja', function (data) {
+        io.emit('testlagi', data);
       });
-      socket.on('clear', function (data) {
-        chat.remove({}, function () {
-          socket.emit('cleared');
-        });
+
+      socket.on('disconnect', () => {
+        console.log('A user disconnected');
       });
     });
   }
@@ -59,9 +45,5 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(router);
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,6 @@ const app = require('../app');
 const router = express.Router();
 const UserController = require('../controllers/user.controller');
 const { authentication } = require('../helpers/jwt');
-router.get('/', (req, res) => {
-  res.send('Hello World!');
-});
 
 router.post('/enterRoom', UserController.enterRoom);
 router.use(authentication);
